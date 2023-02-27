bdcb16b379ab6569fe411efcef47fcb2d749f242
Artur Siedlerski <don.siedler@gmail.com>
2023-02-27 21:27:14 +0100

    Update modals error handling; fix searching for buttons; code reformat

--- main.py
+++ main.py
@@ -21,71 +21,79 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 
 driver.get("https://tinder.com/")
 
-# Proceed to log in
-time.sleep(SLEEP_TIME)
-login_btn = driver.find_element(By.XPATH, '//*[@id="c-60880778"]/div/div[1]/div/main/div['
-                                          '1]/div/div/div/div/header/div/div[2]/div[2]/a')
 
-login_btn.click()
+def login():
+    # Proceed to log in
+    time.sleep(SLEEP_TIME)
+    login_btn = driver.find_element(By.XPATH, '//*[@id="c-60880778"]/div/div[1]/div/main/div['
+                                              '1]/div/div/div/div/header/div/div[2]/div[2]/a')
 
-# Facebook login
-time.sleep(SLEEP_TIME)
-fb_login_btn = driver.find_element(By.XPATH,
-                                   '//*[@id="c160459658"]/main/div/div/div[1]/div/div/div[3]/span/div[2]/button')
-fb_login_btn.click()
+    login_btn.click()
 
-# Switch to Facebook window and allow cookies
-tinder_window = driver.window_handles[0]
-fb_login_window = driver.window_handles[1]
-driver.switch_to.window(fb_login_window)
+    # Facebook login
+    time.sleep(SLEEP_TIME)
+    fb_login_btn = driver.find_element(By.XPATH,
+                                       '//*[@id="c160459658"]/main/div/div/div[1]/div/div/div[3]/span/div[2]/button')
+    fb_login_btn.click()
 
-time.sleep(SLEEP_TIME)
-buttons = driver.find_elements(By.TAG_NAME, "button")
+    # Switch to Facebook window and allow cookies
+    tinder_window = driver.window_handles[0]
+    fb_login_window = driver.window_handles[1]
+    driver.switch_to.window(fb_login_window)
 
-# Find the correct cookie button
-for btn in buttons:
-    if btn.get_attribute("title") == "Only allow essential cookies":
-        btn.click()
+    time.sleep(SLEEP_TIME)
+    buttons = driver.find_elements(By.TAG_NAME, "button")
 
-# Find elements and pass in the credentials
-time.sleep(SLEEP_TIME)
-email_input = driver.find_element(By.ID, "email")
-password_input = driver.find_element(By.ID, "pass")
-sign_in_btn = driver.find_element(By.NAME, "login")
+    # Find the correct cookie button
+    for btn in buttons:
+        if btn.get_attribute("title") == "Only allow essential cookies":
+            btn.click()
 
-email_input.send_keys(FACEBOOK_EMAIL)
-password_input.send_keys(FACEBOOK_PASSWORD)
-sign_in_btn.click()
+    # Find elements and pass in the credentials
+    time.sleep(SLEEP_TIME)
+    email_input = driver.find_element(By.ID, "email")
+    password_input = driver.find_element(By.ID, "pass")
+    sign_in_btn = driver.find_element(By.NAME, "login")
 
-# Switch back to Tinder window: allow location, disable notifications, reject cookies
-driver.switch_to.window(tinder_window)
+    email_input.send_keys(FACEBOOK_EMAIL)
+    password_input.send_keys(FACEBOOK_PASSWORD)
+    sign_in_btn.click()
 
-tinder_loaded = None
-while not tinder_loaded:
-    try:
-        allow_location_btn = driver.find_element(
-            By.XPATH, '//*[@id="c160459658"]/main/div/div/div/div[3]/button[1]/div[2]/div[2]'
-        )
-    except NoSuchElementException:
-        print("Tinder hasn't loaded yet - sleeping for 10s...")
-        time.sleep(10)
-    else:
-        tinder_loaded = True
-        allow_location_btn.click()
+    # Switch back to Tinder window
+    driver.switch_to.window(tinder_window)
 
-time.sleep(SLEEP_TIME)
-disable_notifications_btn = driver.find_element(By.XPATH, '//*[@id="c160459658"]/main/div/div/div/div[3]/button['
-                                                          '2]/div[2]/div[2]')
-disable_notifications_btn.click()
 
-time.sleep(SLEEP_TIME)
-reject_cookies_btn = driver.find_element(By.XPATH, '//*[@id="c-60880778"]/div/div[2]/div/div/div[1]/div['
-                                                   '2]/button/div[2]/div[2]')
-reject_cookies_btn.click()
-time.sleep(5)
+def close_popups():
+    tinder_loaded = None
+    while not tinder_loaded:
+        # Popups: allow location, disable notifications, reject cookies
+        try:
+            allow_location_btn = driver.find_element(
+                By.XPATH, '//*[@id="c160459658"]/main/div/div/div/div[3]/button[1]/div[2]/div[2]'
+            )
+        except NoSuchElementException:
+            print("Tinder hasn't loaded yet - sleeping for 5s...")
+            time.sleep(5)
+        else:
+            tinder_loaded = True
+            allow_location_btn.click()
+
+    time.sleep(SLEEP_TIME)
+    disable_notifications_btn = driver.find_element(By.XPATH, '//*[@id="c160459658"]/main/div/div/div/div[3]/button['
+                                                              '2]/div[2]/div[2]')
+    disable_notifications_btn.click()
+
+    time.sleep(SLEEP_TIME)
+    reject_cookies_btn = driver.find_element(By.XPATH, '//*[@id="c-60880778"]/div/div[2]/div/div/div[1]/div['
+                                                       '2]/button/div[2]/div[2]')
+    reject_cookies_btn.click()
+    time.sleep(2)
+
+
+login()
+close_popups()
 
 cards_available = True
-
 while cards_available:
 
     # Try to locate the card. Go to sleep if not available
@@ -93,14 +101,24 @@ while cards_available:
     retries = 0
 
     while not card_loaded:
+        # Check for cards
+        try:
+            beacon = driver.find_element(By.CSS_SELECTOR, "div.beacon")
+        except NoSuchElementException:
+            card_loaded = True
+        else:
+            print("No cards available! Refreshing...")
+            driver.refresh()
+            time.sleep(10)
 
         # Refresh the page after 5 retries to load a card.
         if retries == 3:
             print("That didn't work - refreshing the page...")
             driver.refresh()
+            time.sleep(10)
 
         try:
-            time.sleep(10)
+            time.sleep(5)
             bullets_div = driver.find_element(By.XPATH,
                                               '//*[@id="c-60880778"]/div/div[1]/div/main/div[1]/div/div/div[1]/div['
                                               '1]/div/div[2]/div[1]/div[2]'
@@ -110,20 +128,37 @@ while cards_available:
         except NoSuchElementException:
             print("Couldn't find any bullets - checking if this is a single picture card...")
             try:
-                btns = driver.find_elements(By.CSS_SELECTOR, "span.Hidden")
-                for btn in btns:
-                    if btn.text == "Like":
-                        like_btn = btn
-                    elif btn.text == "Nope":
-                        nope_btn = btn
-                if not like_btn and not nope_btn:
-                    print("Neither of the available buttons is like/hope - going ahead...")
+                like_btn = driver.find_element(
+                    By.XPATH,
+                    "/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[4]/button"
+                )
+
+                nope_btn = driver.find_element(
+                    By.XPATH,
+                    "/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[3]/div/div[2]/button"
+                )
 
             except NoSuchElementException:
-                # Couldn't find the buttons - card hasn't been loaded yet - sleeping for 5s...
-                print("Couldn't find any buttons - sleeping for 5s and retrying..")
-                time.sleep(5)
-                retries += 1
+                btns = driver.find_elements(By.CSS_SELECTOR, "button span span span.Hidden")
+                like_btn = None
+                nope_btn = None
+
+                for btn in btns:
+                    print(btn.text)
+                    if btn.text == "LIKE":
+                        like_btn = btn
+                        print("Like button found!")
+                    elif btn.text == "NOPE":
+                        nope_btn = btn
+                        print("Nope button found!")
+                if not like_btn and not nope_btn:
+                    # Couldn't find the buttons - card hasn't been loaded yet - sleeping for 5s...
+                    print("Couldn't find like/nope buttons - sleeping for 5s and retrying..")
+                    time.sleep(5)
+                    retries += 1
+                else:
+                    nope_btn.click()
+
             else:  # Try block successful
                 if like_btn and nope_btn:
                     print("Like/nope buttons found. Click!")
@@ -148,26 +183,40 @@ while cards_available:
     for index, bullet in enumerate(bullets):
         try:
             bullet.click()
-        except ElementNotInteractableException:  # Catch preview
+
+        except ElementNotInteractableException:  # Preview still displaying
             print("Preview detected! Breaking the loop...")
             break
-        except ElementClickInterceptedException:  # 'Add to Home Screen' modal
-            print("Annoying modal detected! Trying to close it...")
-            time.sleep(2)
-            try:
-                close_modal_btn = driver.find_element(By.XPATH, "/html/body/div[2]/main/div/div[2]/button[2]")
-                close_modal_btn.click()
-            except ElementClickInterceptedException:  # 'Out of likes' modal
-                print("Looks like we're out of likes! Closing the modal and the browser.")
-                close_modal_btn = driver.find_element(By.XPATH, "/html/body/div[2]/main/div/div[3]/button[2]")
-                close_modal_btn.click()
-                time.sleep(10)
-                driver.quit()
+
+        except ElementClickInterceptedException:  # Modal found
+            print("Modal detected! Figuring out what it is...")
+            time.sleep(1)
+
+            try:  # "Add to home screen' modal error handler
+                close_home_screen_modal_btn = driver.find_element(
+                    By.XPATH, "/html/body/div[2]/main/div/div[2]/button[2]"
+                )
+                close_home_screen_modal_btn.click()
+
+            except NoSuchElementException:  # Not a 'home screen' modal!
+                try:  # "Out of likes' modal error handler
+                    close_modal_btn = driver.find_element(By.XPATH, "/html/body/div[2]/main/div/div[3]/button[2]")
+                    print("Looks like we're out of likes! Closing the modal and the browser.")
+                    close_modal_btn.click()
+                    time.sleep(10)
+                    driver.quit()
+                except NoSuchElementException:  # Not a 'Out of likes' modal!
+                    print("Looks like it's a match! Trying to close the modal...")
+                    close_modal_btn = driver.find_element(
+                        By.XPATH, "/html/body/div[1]/div/div[1]/div/main/div[2]/main/div/div[1]/div/div[4]/button"
+                    )
+                    close_modal_btn.click()
+
         else:
             print(f"Pic #{index + 1} - sleeping for 3s...")
             time.sleep(3)
 
-    time.sleep(3)
+    time.sleep(1)
 
     try:
         like_btn = driver.find_element(
@@ -179,10 +228,11 @@ while cards_available:
         )
     except NoSuchElementException:
         print("Couldn't find like/nope button :( Trying an alternative...")
-        for btn in driver.find_elements(By.CSS_SELECTOR, "span.Hidden"):
-            if btn.text == "Like":
+        for btn in driver.find_elements(By.CSS_SELECTOR, "button span span span.Hidden"):
+            print(btn.text)
+            if btn.text == "LIKE":
                 like_btn = btn
-            elif btn.text == "Nope":
+            elif btn.text == "NOPE":
                 nope_btn = btn
 
-    like_btn.click()
+    nope_btn.click()
